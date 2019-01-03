50bbd1299d468a1c9334ea128f710505d280f94c
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 17:30:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,11 +15,13 @@ def first_challenge
 
   #your code here
   # contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
-  contacts.collect do |key,value|
-    if key == "Freddy Mercury"
-      if value == :favorite_icecream_flavors
-      value.delete("strawberry")
-    end
+  contacts.collect do |person,data|
+    data.collect do |attribute,value|
+      if attribute == :favorite_icecream_flavors
+        value.collect do |flavor|
+          if flavor == "strawberry"
+            value.delete(flavor)
+          end
     end
   end
   # contacts["Freddy Mercury"].delete_if {|key,value| :favorite_icecream_flavors >= "strawberry"}
